9ad8e1dc145a2ae76426f630dccba03aff57a4c9
meldsza <meldsza@gmail.com>
2017-04-17 22:12:53 +0530

    look for guildMemberUpdate

--- bot/modules/verify.js
+++ bot/modules/verify.js
@@ -4,8 +4,10 @@ const bot = require('./../bot');
 const verify = require('./../lib/verify');
 const reject = require('./../lib/reject');
 const settings = require('./../../settings.json');
-bot.on('guildMemberAdd', (m) => {
+bot.on('guildMemberUpdate', (om,m) => {
     //if (m.guild.id != settings.bughunterGaming) return;
+    if(om.roles.find('name','Member'))return;
+    if(!m.roles.find('name','Member'))return;
     console.log(settings['discord-testers']);
     console.log( userbot.guilds.get(settings['discord-testers']).name);
     let mem = userbot.guilds.get(settings['discord-testers']).member(bot.users.get(m.user.id));
