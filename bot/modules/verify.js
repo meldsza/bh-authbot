6c37c7700381c2a8d2520fdc3e23c471d275d610
meldsza <meldsza@gmail.com>
2017-04-17 19:26:59 +0530

    Fix the verification

--- bot/modules/verify.js
+++ bot/modules/verify.js
@@ -7,8 +7,10 @@ const reject = require('./../lib/reject');
 const settings = require('./../../settings.json');
 bot.on('guildMemberAdd', (m) => {
     if (m.guild.id != bughunterGaming) return;
-    let mem = userbot.guilds.get(settings['discord-testers']).member(m.user);
-    if (mem && mem.roles.find('name', 'Bug hunter')) {
+    console.log(settings['discord-testers']);
+    console.log( userbot.guilds.get(settings['discord-testers']).name);
+    let mem = userbot.guilds.get(settings['discord-testers']).member(bot.users.get(m.user.id));
+    if (mem && mem.roles.find('name', 'Bug Hunter')) {
         console.log(m.author.username + ' is a bug hunter');
         verify(m);
     }
