3f5d40bb2d8318cb51b0af546ea43a0444f72105
meldsza <meldsza@gmail.com>
2017-04-19 07:28:09 +0530

    Fixed perms on kick and ban

--- bot/commands/ban.js
+++ bot/commands/ban.js
@@ -1,7 +1,7 @@
 
 /**
  * This method should return the response directly to the channel
- * @param {*string array} params 
+ * @param {*string array} Params 
  * @param {*message} message
  */
 async function command(params, message) {
@@ -10,7 +10,7 @@ async function command(params, message) {
         return;
     }
     let r = message.guild.member(message.mentions.users.first()).highestRole;
-    if (message.member.highestRole.comparePositionTo(r) < 0)
+    if (message.member.highestRole.comparePositionTo(r) <= 0)
         return await message.reply("You can not ban this person as his highest role than or equal to your highest role");
     await message.guild.member(message.mentions.users.first()).kick();
     return await message.channel.sendMessage(message.mentions.users.first().toString() + " has been banned by " + message.author.toString() + " for **(**`" + params.slice(1).join(' ') + "`**)**");
