5e02af64ff441d6c3bfabb8fadc092e95065a3cc
meldsza <meldsza@gmail.com>
2017-04-19 07:26:39 +0530

    Update kick.js

--- bot/commands/kick.js
+++ bot/commands/kick.js
@@ -10,7 +10,7 @@ async function command(params, message) {
         return;
     }
     let r = message.guild.member(message.mentions.users.first()).highestRole;
-    if (message.member.highestRole.comparePositionTo(r) < 0)
+    if (message.member.highestRole.comparePositionTo(r) <= 0)
         return await message.reply("You can not kick this person as his highest role than or equal to your highest role");
     await message.guild.member(message.mentions.users.first()).kick();
     return await message.channel.sendMessage(message.mentions.users.first().toString() + " has been kicked by " + message.author.toString() + " for **(**`" + params.slice(1).join(' ') + "`**)**");
