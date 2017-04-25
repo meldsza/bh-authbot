a7d6e2e2d8fea944dc5fd03d1eb5ad3dd690b28e
meldsza <meldsza@gmail.com>
2017-04-25 19:46:25 +0400

    Create message2embed.js

---
+++ bot/lib/message2embed.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+module.exports = (message) => {
+    const embed = new Discord.RichEmbed();
+    embed.setAuthor(message.author.username,message.author.avatarURL)
+    embed.setDescription(message.content);
+    embed.setFooter(message.createdAt)
+    //embed.setTitle("Quote");
+    return embed;
+}
