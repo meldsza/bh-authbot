c771452a857d32cdd949e1840a8c53284a29dab5
meldsza <meldsza@gmail.com>
2017-04-25 19:45:37 +0400

    add quote.js

---
+++ bot/commands/quote.js
@@ -0,0 +1,42 @@
+const Message = require('./../../orm/discord_messages');
+const m2e = require('./../lib/message2embed');
+const bot = require('./../bot');
+/**
+ * This method should return the response directly to the channel
+ * @param {*string array} params 
+ * @param {*message} message
+ */
+async function command(params, message) {
+   if (params.length < 1)
+        return message.reply("incorrect usage\nSyntax: ~quote <messageID>");
+   let m =await Message.where('messageID', params[0]).fetch();
+   if (m) {
+        console.log(m);
+        let ch = bot.channels.get(m.attributes.channel);
+        m = await ch.fetchMessage(m.attributes.messageID);
+    }
+    else
+    {
+        m = await message.channel.fetchMessage(params[0]);
+        if(!m)
+            return message.reply("message not available");
+    }
+    console.log("fetching : "+m.id);
+    let attach = m.attachments.first();
+    m =m2e(m);
+    console.log(m);
+    if(attach)
+        attach = attach.url;
+    message.channel.sendEmbed(m,{file:attach}).catch(console.error);
+}
+/**
+ * description of the command
+ */
+const description = "quotes a message via id";
+/**
+ * Define Exports
+ */
+module.exports = {
+    execute: command,
+    description: description
+};
