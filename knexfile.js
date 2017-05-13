18af2c66a6b45d4a18fe4d474d77bb857a5ef536
meldsza <melroynd@live.com>
2017-05-13 09:59:21 +0530

    move to sqlite

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,9 @@
 // Update with your config settings.
 
 module.exports = {
-  client: "mssql",
+  client: "sqlite",
   connection: {
-    host: process.env.DB_HOST,
-    user: "melroy",
-    password: process.env.DB_PASS,
-    database: "discord",
-    options: {
-      port: 1433,
-      encrypt: true
-    }
+    filename: './dev.sqlite3'
   },
   migrations: {
     tableName: 'knex_migrations'
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+knex migrate:latest
+node index.js
