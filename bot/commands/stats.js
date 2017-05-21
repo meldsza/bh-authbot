845681ca8ea52699b7f0dd798d3274f44bab3f50
Melroy Neil Dsouza <meldsza@gmail.com>
2017-05-21 21:33:01 +0530

    fix stats command

--- bot/commands/stats.js
+++ bot/commands/stats.js
@@ -11,15 +11,17 @@ async function command(params, message) {
         .setThumbnail(guild.iconURL)
         .setTitle(guild.name)
         .setAuthor(guild.owner.user.username, guild.owner.user.displayAvatarURL)
-        .setFooter("formed on " + guild.createdAt)
+        .setTimestamp(guild.createdAt)
         .addField('Members: ', guild.memberCount, true)
         .addField('Online: ', guild.presences.map(p => p.status != 'offline').length, true)
         .addField('Region: ', guild.region, true);
-    guild.roles.map((role) => {
-        if (role.name != '@everyone')
-            embed.addField(role.name + ": ", role.members.size, true);
-    });
-    return message.channel.sendEmbed(embed).catch(message.channel.sendMessage);
+    let r ='__**Roles**__:\n'
+    guild.roles.map((role) =>{
+		if(role.name !='@everyone')
+		r = r + "**"+role.name+"**: "+role.members.size+'\n';
+	});
+	embed.setDescription (r);
+    return message.channel.send('', { embed: embed }).catch(message.channel.send);
 }
 /**
  * description of the command
