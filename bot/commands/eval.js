1f877afc728ded4eb6ae66321495c31e99754912
Melroy Neil Dsouza <meldsza@gmail.com>
2017-05-21 19:47:28 +0530

    Create eval.js

---
+++ bot/commands/eval.js
@@ -0,0 +1,46 @@
+
+/**
+ * This method should return the response directly to the channel
+ * @param {*string array} params 
+ * @param {*message} message
+ */
+async function command(params, message) {
+  if (!settings.owners.includes(message.author.id)) {
+        return await message.reply('Not worthy')
+  }
+  code = msg.content.trim().substring(5).trim();
+	const time = process.hrtime();
+	try {
+		var evaled = eval(code);
+		if (typeof evaled !== 'string')
+			evaled = require('util').inspect(evaled);
+		msg.channel.send("**Input**\n```JS\n" + code + "```\n**OUTPUT**\n```xl\n" +
+			clean(evaled).replace(bot.token,"YOUCANTSEEMYTOKENFORAREASON") +
+			"\n```\n**Execution Time**: " + (process.hrtime(time)[1]/1000000)+"ms"
+			, { split: true }).catch(msg.edit);
+	}
+	catch (err) {
+		msg.send("**Input**\n```JS\n" + code + "```\n`ERROR` ```xl\n" +
+			clean(err) + "\n```\n**Execution Time**: " + (process.hrtime(time)[1]/1000000) +
+			"ms\n```", { split: true });
+	}
+}
+function clean(text) {
+	if (typeof (text) === "string") {
+		return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203)).substr(0, 1980);
+	}
+	else {
+		return text.toString().substr(0, 1980);
+	}
+}
+/**
+ * description of the command
+ */
+const description = "says hi";
+/**
+ * Define Exports
+ */
+module.exports = {
+    execute: command,
+    description: description
+};
