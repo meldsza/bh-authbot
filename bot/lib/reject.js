cefd4f02298aa56d6e8729238da18e8adc1c0002
Melroy Neil Dsouza <meldsza@gmail.com>
2017-10-03 07:55:13 +0530

    Part 2

--- bot/lib/reject.js
+++ bot/lib/reject.js
@@ -2,5 +2,5 @@ const bot = require('./../bot.js');
 const settings = require('./../../settings.json')
 module.exports = async function (message) {
     let m = await message.reply("Sorry, but you cannot be added as a bug hunter because you lack the role in discord testers server");
-    m.delete(5000);
+    setTimeout(m.delete,5000);
 }
